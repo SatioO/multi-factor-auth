97e55971303c93a323b239a79352161ecb3bf49a
vaibhav.satam <vaibhav.satam@accionlabs.com>
2022-06-08 11:01:41 +0530

    added authentication exception handler class

--- src/main/java/com/ifsg/multifactorauth/models/dtos/Metadata.java
+++ src/main/java/com/ifsg/multifactorauth/models/dtos/Metadata.java
@@ -1,12 +1,18 @@
 package com.ifsg.multifactorauth.models.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Metadata {
+    @JsonProperty("GLOBALUUID")
     private String GLOBALUUID;
+
+    @JsonProperty("REQUESTUUID")
     private String REQUESTUUID;
+
+    @JsonProperty("CHANNEL")
     private String CHANNEL;
 }
